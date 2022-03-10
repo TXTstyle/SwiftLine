e2d3c1931b49adc38585f0d54769900cd6c71da8
Oskar Engström <engstrom.oskar@elev.ga.ntig.se>
2022-03-10 12:50:55 +0100

    Heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+const PORT = process.env.PORT || 3000
 
 const express = require("express");
 const multer = require('multer')
@@ -182,6 +183,6 @@ app.use((req, res, next) => {
 })
 
 
-app.listen(2000, () => {
+app.listen(PORT, () => {
     console.log("server running!")
 })
