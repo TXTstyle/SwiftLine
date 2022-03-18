757e2ad64cdf123de0bad9a2ed3a2ba53660a451
Oskar <oskar.fv57@gmail.com>
2022-03-18 21:29:14 +0100

    media query

--- public/js/dark.js
+++ public/js/dark.js
@@ -1,5 +1,30 @@
-let root = document.documentElement;
+const root = document.documentElement;
 
 root.style.setProperty('--background-color', '#121212');
 root.style.setProperty('--surface-color', "#181818");
 root.style.setProperty('--font-color', "#ccc");
+
+
+function openTab(n,c) {
+    const sideMenu = document.querySelector(n);
+    if (sideMenu.style.display == 'none') {
+        sideMenu.style.display = "flex";
+        sideMenu.toggleAttribute('closed');
+        for (let i = 0; i < sideMenu.children.length; i++) {
+            const child = sideMenu.children[i];
+            if (child.classList.contains(c)) {
+                child.style.display = "flex";
+                continue;
+            }
+            child.style.display = "block";
+        }
+    } else {
+        sideMenu.style.display = "none";
+        sideMenu.toggleAttribute('closed');
+        for (let i = 0; i < sideMenu.children.length; i++) {
+            const child = sideMenu.children[i];
+            
+            child.style.display = "none";
+        }
+    }
+}
